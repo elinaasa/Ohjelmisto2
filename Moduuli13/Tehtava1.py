c5da932b8afd5025861c015533fa5a516e796cc9
Omistaja <sankilaelina@gmail.com>
2022-11-21 11:42:27 +0200

    valmis

--- Moduuli13/Tehtava1.py
+++ Moduuli13/Tehtava1.py
@@ -3,49 +3,33 @@
 # Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:5000/alkuluku/31.
 # Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.
 
-from flask import Flask, request
-
-luku = int(input("Syötä kokonaisluku: "))
-
-for i in range(2, luku / 2 + 1):
-    if luku % 2 == 0:
-        print("Luku ei ole alkuluku.")
-    else:
-        print("Luku on alkuluku.")
-
 from flask import Flask, Response
 import json
 
 app = Flask(__name__)
-@app.route('/alkuluku/<luku>/)
+
+
+@app.route('/alkuluku/<luku>/')
 def alkuluku(luku):
     try:
         luku = int(luku)
+        for i in range(2, luku / 2 + 1):
+            if luku % 2 == 0:
+                response_dict = {"Number": luku, "isPrime": False}
+                response_json = json.dumps(response_dict)
+                return Response(response=response_json, status=200, mimetype="application/json")
+            else:
+                response_dict = {"Number": luku, "isPrime": True}
+                response_json = json.dumps(response_dict)
+                return Response(response=response_json, status=200, mimetype="application/json")
 
-        tilakoodi = 200
-        vastaus = {
-            "status": tilakoodi,
-            "luku": luku
-        }
+
+
 
     except ValueError:
-        tilakoodi = 400
-        vastaus = {
-            "status": tilakoodi,
-            "teksti": "Virheellinen syöte."
-        }
+        response_json = json.dumps("Virheellinen syöte.")
+        return Response(response=response_json, mimetype="application/json")
 
-    jsonvast = json.dumps(vastaus)
-    return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
-
-@app.errorhandler(404)
-def page_not_found(virhekoodi):
-    vastaus = {
-        "status" : "404",
-        "teksti" : "Virheellinen päätepiste"
-    }
-    jsonvast = json.dumps(vastaus)
-    return Response(response=jsonvast, status=404, mimetype="application/json")
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
